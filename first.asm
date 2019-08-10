a63e9146da1083a191da5ee770c4a9c1b2090355
vageli <vageli.code@gmail.com>
2019-08-10 15:28:01 -0400

    crashing!

--- first.asm
+++ first.asm
@@ -7,7 +7,7 @@ repeat:
     mov al,[bx]
     test al,al
     je end
-    mov ah,0x0e
+    mov al,0x0e ; this caused a crash
     ; mov bx,0x000f ; modifying this value has no consequence in DOSBox
     int 0x10
     inc bx
