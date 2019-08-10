2c91bcd17c426cfcd283e3f3a31bc9bfe2b81162
vageli <vageli.code@gmail.com>
2019-08-10 15:27:53 -0400

    cant set graphics mode?

--- first.asm
+++ first.asm
@@ -8,7 +8,7 @@ repeat:
     test al,al
     je end
     mov ah,0x0e
-    mov bx,0x000f
+    ; mov bx,0x000f ; modifying this value has no consequence in DOSBox
     int 0x10
     inc bx
     jmp repeat
